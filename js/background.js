141cfb9db113e8fb2ff2eaa43890caafa61b4cd7
seungwooj <seungwoo.aaron@gmail.com>
2021-08-11 23:46:48 +0900

    change css

--- js/background.js
+++ js/background.js
@@ -17,3 +17,4 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+bgImage.id = "bgImage";
--- js/weather.js
+++ js/weather.js
@@ -8,7 +8,7 @@ function getWeatherAndCity(lat, lon) {
 		.then((data) => {
 			const weather = document.querySelector("#weather span:first-child");
 			const city = document.querySelector("#weather span:last-child");
-			weather.innerText = `${data.weather[0].main} Celcius @ ${data.main.temp}`;
+			weather.innerText = `${data.weather[0].main} / ${data.main.temp} Celcius @`;
 			city.innerText = data.name;
 		});
 }
